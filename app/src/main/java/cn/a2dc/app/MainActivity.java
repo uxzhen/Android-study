fb8b25b335b3950812b629ab013fb9304492b3ec
2554463433@qq.com <2554463433@qq.com>
2021-01-10 23:48:42 +0800

    202001102348按钮点击事件

--- app/src/main/java/cn/a2dc/app/MainActivity.java
+++ app/src/main/java/cn/a2dc/app/MainActivity.java
@@ -1,19 +1,35 @@
 package cn.a2dc.app;
 
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnOne,btnTwo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.edit_test);
+        setContentView(R.layout.layout_btn);
         /*获取焦点*/
-        EditText etName = findViewById(R.id.etName);
-        etName.requestFocus();
+        //EditText etName = findViewById(R.id.etName);
+        //etName.requestFocus();
         //获取第二个文字
-        etName.setSelection(2);
+        //etName.setSelection(2);
+        btnOne = (Button) findViewById(R.id.btnOne);
+        btnTwo = (Button) findViewById(R.id.btnTwo);
+        btnTwo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(btnTwo.getText().toString().equals("按钮不可用")){
+                    btnOne.setEnabled(false);
+                    btnTwo.setText("按钮可用");
+                }else{
+                    btnOne.setEnabled(true);
+                    btnTwo.setText("按钮不可用");
+                }
+            }
+        });
     }
 }
